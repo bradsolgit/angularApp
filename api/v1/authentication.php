845fd36826552ff4d93765b0bbee836ff1ccc715
bradsolws <rohitbhattad@gmail.com>
2015-06-09 18:07:23 +0530

    Initial  Commit

---
+++ api/v1/authentication.php
@@ -0,0 +1,77 @@
+<?php 
+$app->get('/session', function() {
+    $db = new DbHandler();
+    $session = $db->getSession();
+    $response["uid"] = $session['uid'];
+    $response["email"] = $session['email'];
+    $response["name"] = $session['name'];
+    echoResponse(200, $session);
+});
+
+$app->post('/login', function() use ($app) {
+    require_once 'passwordHash.php';
+    $r = json_decode($app->request->getBody());
+    verifyRequiredParams(array('email', 'password'),$r->customer);
+    $response = array();
+    $db = new DbHandler();
+    $password = $r->customer->password;
+    $email = $r->customer->email;
+    $user = $db->getOneRecord("select uid,name,password,email,created from customers_auth where phone='$email' or email='$email'");
+    if ($user != NULL) {
+        if(passwordHash::check_password($user['password'],$password)){
+        $response['status'] = "success";
+        $response['message'] = 'Logged in successfully.';
+        $response['name'] = $user['name'];
+        $response['uid'] = $user['uid'];
+        $response['email'] = $user['email'];
+        $response['createdAt'] = $user['created'];
+        if (!isset($_SESSION)) {
+            session_start();
+        }
+        $_SESSION['uid'] = $user['uid'];
+        $_SESSION['email'] = $email;
+        $_SESSION['name'] = $user['name'];
+        } else {
+            $response['status'] = "error";
+            $response['message'] = 'Login failed. Incorrect credentials';
+        }
+    }else {
+            $response['status'] = "error";
+            $response['message'] = 'No such user is registered';
+        }
+    echoResponse(200, $response);
+});
+	
+	
+$app->post('/dashboard', function() use ($app) {
+    $response = array();
+    $db = new DbHandler();
+    
+    if(true){
+        $tabble_name = "angularcode_customers";
+        $result = $db->getAllRecords("select * from angularcode_customers");
+        if ($result != NULL) {
+        	$response['status'] = "success";
+        	$response['message'] = 'Logged in successfully.';
+        	$response['customers'] = $result;
+        	//$response = $result;
+            echoResponse(200, $response);
+        } else {
+            $response["status"] = "error";
+            $response["message"] = "Failed to create customer. Please try again";
+            echoResponse(201, $response);
+        }            
+    }else{
+        $response["status"] = "error";
+        $response["message"] = "An user with the provided phone or email exists!";
+        echoResponse(201, $response);
+    }
+});
+$app->get('/logout', function() {
+    $db = new DbHandler();
+    $session = $db->destroySession();
+    $response["status"] = "info";
+    $response["message"] = "Logged out successfully";
+    echoResponse(200, $response);
+});
+?>
---
+++ api/v1/customerDetails.php
@@ -0,0 +1,40 @@
+<?php 
+$app->get('/session', function() {
+    $db = new DbHandler();
+    $session = $db->getSession();
+    $response["uid"] = $session['uid'];
+    $response["email"] = $session['email'];
+    $response["name"] = $session['name'];
+    echoResponse(200, $session);
+});
+
+$app->get('/customers', function() use ($app) {
+    require_once 'passwordHash.php';
+    $r = json_decode($app->request->getBody());
+    verifyRequiredParams(array('email', 'password'),$r->customer);
+    $response = array();
+    $db = new DbHandler();
+    $password = $r->customer->password;
+    $email = $r->customer->email;
+    $user = $db->getOneRecord("select uid,name,password,email,created from customers_auth where phone='$email' or email='$email'");
+    if ($user != NULL) {
+        if(passwordHash::check_password($user['password'],$password)){
+        $response['status'] = "success";
+        $response['message'] = 'Logged in successfully.';
+        $response['name'] = $user['name'];
+        $response['uid'] = $user['uid'];
+        $response['email'] = $user['email'];
+        $response['createdAt'] = $user['created'];
+       
+        } else {
+            $response['status'] = "error";
+            $response['message'] = 'Login failed. Incorrect credentials';
+        }
+    }else {
+            $response['status'] = "error";
+            $response['message'] = 'No such user is registered';
+        }
+    echoResponse(200, $response);
+});
+
+?>
